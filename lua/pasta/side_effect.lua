8b2c83da41f59a915bc6c0d01b89f4c2d4dff9a4
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2024-05-17 22:58:11 +0900

    don't use diagnostic.disable

--- lua/pasta/side_effect.lua
+++ lua/pasta/side_effect.lua
@@ -1,9 +1,10 @@
 local SideEffect = {}
 
 function SideEffect.prevent(callback)
-  vim.diagnostic.disable(0)
+  local is_enabled = vim.diagnostic.is_enabled({ bufnr = 0 })
+  vim.diagnostic.enable(false, { bufnr = 0 })
   local ok, res = pcall(callback)
-  vim.diagnostic.enable(0)
+  vim.diagnostic.enable(is_enabled, { bufnr = 0 })
   if not ok then
     error(res)
   end
