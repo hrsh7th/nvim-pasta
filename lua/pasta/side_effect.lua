a49701293ae1018ecb47f1e7be821d6536eefbde
Mikhail Ulyanov <m.ulyanov@yadro.com>
2024-10-23 22:39:38 +0300

    Fix diagnostic sideeffect

--- lua/pasta/side_effect.lua
+++ lua/pasta/side_effect.lua
@@ -1,9 +1,10 @@
 local SideEffect = {}
 
 function SideEffect.prevent(callback)
-  vim.diagnostic.enable(false)
+  local diagnostic_active = vim.diagnostic.is_enabled()
+  if diagnostic_active then vim.diagnostic.enable(false) end
   local ok, res = pcall(callback)
-  vim.diagnostic.enable(true)
+  if diagnostic_active then vim.diagnostic.enable(true) end
   if not ok then
     error(res)
   end
